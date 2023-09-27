d32d0623536e467b19f17d9f7ed7fe669588884c
TomaszSzlotawa <127597580+TomaszSzlotawa@users.noreply.github.com>
2023-09-27 12:00:02 +0200

    added navbar and made navigation links and created sites

--- Club/App/urls.py
+++ Club/App/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from App.views import main, main_page
+from App.views import main, main_page, news,team
 
 urlpatterns = [
     path('main',main, name='main'),
     path('main_page',main_page, name='main_page'),
+    path('news',news, name='news'),
+    path('team',team, name='team'),
 ]
--- Club/App/views.py
+++ Club/App/views.py
@@ -6,4 +6,10 @@ def main(request):
     return HttpResponse('Strona główna')
 
 def main_page(request):
-    return render (request,"main.html",)
+    return render (request,"main_page.html",)
+
+def news(request):
+    return render (request,"news.html",)
+
+def team(request):
+    return render (request,"team.html",)
