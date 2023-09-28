adff21cd8756232e7427a492c84e8beef0f1ff99
TomaszSzlotawa <127597580+TomaszSzlotawa@users.noreply.github.com>
2023-09-28 21:56:58 +0200

    fixed media files

--- Club/Club/settings.py
+++ Club/Club/settings.py
@@ -121,6 +121,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = ['static_files']
 
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = 'media_files'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
